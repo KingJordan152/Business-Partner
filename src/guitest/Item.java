270a3a46e723a491a44472bd1f7a3b57efc7b82a
Elijah Price <46971329+Elijah3502@users.noreply.github.com>
2019-05-10 09:15:10 -0700

    Changes to the Table in the main menu

---
+++ src/guitest/Item.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guitest;
+
+/**
+ *
+ * @author elija
+ */
+public class Item 
+{
+    String name, company;
+    int quant;
+    public Item(String medName, String compName, int quantity)
+    {
+        name = medName;
+        company = compName;
+        quant = quantity;
+    }
+    
+   public String getMedName()
+   {
+       return name;
+   }
+    
+}
